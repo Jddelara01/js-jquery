6877ea6a0bef1016633e109fa4e3fa09b526ec47
Jddelara01 <jddelara01@gmail.com>
2023-01-25 10:26:25 +0000

    Add css with jquery lessons

--- 02-jQueryIntroduction/02-downloading_jquery/script.js
+++ 02-jQueryIntroduction/02-downloading_jquery/script.js
@@ -18,3 +18,28 @@ $(document).ready(function() {
         $(".stream3").addClass('highlight_stream');
    });
 });
+
+/* -------------------------------------------------CSS with jQuery */
+
+// check the background color of <p> elements
+$("p").css("background-color");
+
+// check the font of <p> elements
+$("p").css("font-family");
+
+// change the css of any element
+$("li").css("color", "red"); // change the text of all <li> to red
+$("h2").css("text-decoration", "underline");
+$("ul").css("border", "solid 1px #ccc"); // adds a 1px wide border to all lists
+
+$("#my_footer").html(); // retrieves all html from "my_footer" id
+$("body").html(); // retrieves all html of the body
+
+// change html content using jQuery
+$("#my_footer").html("<h1>This is my fancy new content. Thanks jQuery</h1>"); // replaced the <p> in the "my_footer" id
+
+// replace the text content of an element
+$("#my_footer").text("This is my new fancy text. Thanks again jQuery.");
+
+// insert new element
+$("#my_footer").append(" <span>&copy; 2023.</span>");
